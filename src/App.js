95aaa06de0b54ede5762cd1648d7629fe9c6dba9
Amina <omirzakaminax@gmail.com>
2024-08-04 09:37:03 +0500

    last commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Facilities from './pages/Facilities';
 import Support from './pages/Support';
-import DataMonitoring from './pages/DataMonitoring';
-import ContactUs from './pages/ContactUs';
 import Technology from './pages/Technology';
+import ContactUs from './pages/ContactUs';
+import Features from './pages/Features';
 import HowItWorks from './pages/HowItWorks';
 import UserDashboard from './pages/UserDashboard';
 import UserGuide from './pages/UserGuide';
 import FAQ from './pages/FAQ';
+import './App.css';
 
 const App = () => {
     return (
@@ -22,9 +23,9 @@ const App = () => {
                 <Route path="/about" element={<About />} />
                 <Route path="/facilities" element={<Facilities />} />
                 <Route path="/support" element={<Support />} />
-                <Route path="/data-monitoring" element={<DataMonitoring />} />
                 <Route path="/technology" element={<Technology />} />
                 <Route path="/contactus" element={<ContactUs />} />
+                <Route path="/features" element={<Features />} />
                 <Route path="/howitworks" element={<HowItWorks />} />
                 <Route path="/userdashboard" element={<UserDashboard />} />
                 <Route path="/userguide" element={<UserGuide />} />
